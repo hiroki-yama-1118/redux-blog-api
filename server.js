7e9b4a0e365f1738dc1aa5734338ebea339f4c37
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-05-16 09:34:59 +0900

    heroku deploy

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 require("dotenv").config({ debug: true });
-import express, { json } from "express";
-import { connect, connection } from "mongoose";
-import cors from "cors";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = 8000;
 const env = process.env;
 const uri = env.MONGODB_URI;
 
 //MONCODBと繋げる
-connect(uri);
-connection.once("open", () => {
+mongoose.connect(uri);
+mongoose.connection.once("open", () => {
   console.log("db connected");
 });
 
 //コースエラー回避
 app.use(cors());
 //express使う
-app.use(json());
+app.use(express.json());
 
 //それぞれのページのroutesを指定
-import usersRouter from "./routes/users";
-import blogsRouter from "./routes/blogs";
-import booksRouter from "./routes/books";
-import s3urlRouter from "./routes/s3url";
+const usersRouter = require("./routes/users");
+const blogsRouter = require("./routes/blogs");
+const booksRouter = require("./routes/books");
+const s3urlRouter = require("./routes/s3url");
 
 //ルーターに読み込まれる
 app.use("/users", usersRouter);
