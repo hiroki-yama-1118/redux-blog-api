046ef3b411ed3867bd49f4103f57766c21741b63
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-05-16 13:06:46 +0900

    CORSを有効化

--- server.js
+++ server.js
@@ -30,6 +30,16 @@ app.use("/blogs", blogsRouter);
 app.use("/books", booksRouter);
 app.use("/s3url", s3urlRouter);
 
+// CORSを有効化;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 //テスト
 app.get("/", (req, res) => {
