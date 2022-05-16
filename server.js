b9621253c50510b1af20c67ae3703f35d7aaef2b
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-05-16 13:56:18 +0900

    heroku deploy

--- server.js
+++ server.js
@@ -20,12 +20,15 @@ app.use(express.json());
 
 // CORSを有効化;
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
   next();
 });
 
